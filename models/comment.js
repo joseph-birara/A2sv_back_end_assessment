57b0c8db1ae7b8ef828d8d7c7f6f4eb5b22ae225
joseph-birara <josephbirara92@gmail.com>
2024-07-24 15:59:37 +0300

    feat:add schema

---
+++ models/comment.js
@@ -0,0 +1,21 @@
+//Add Comment: Users can add comments to recipes with the following fields:
+// Content
+// Date
+// Author
+
+
+
+const mongoose = require('mongoose');
+
+const Comment = new mongoose.Schema({
+    content: { type:String, required: true },
+    Author :{ type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+    Recipe : { type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }
+
+})
+
+
+const comment = mongoose.model('Recipe', Comment);
+module.exports = comment;
+
+
